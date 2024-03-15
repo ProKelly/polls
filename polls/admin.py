036448a8907d4e6dec6db1f4fa3a4af04e8e608d
prokelly <anyeprincekelly816@gmail.com>
2024-03-15 06:17:02 -0700

    customizing the admin looks and i've some static files in the polls app

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register([Question, Choice])
+
+class QuestionAdmin(admin.ModelAdmin):
+   fieldsets = [
+      (None, {'fields':['question_text',]}),
+      ('Date information', {'fields':['pub_date']})
+   ]
+
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
