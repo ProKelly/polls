6e2618e155678888a701d2d80e31a8d66bbb87e7
prokelly <anyeprincekelly816@gmail.com>
2024-03-15 05:50:34 -0700

    finished writing the varius test cases for my models and view funtions

--- polls/tests.py
+++ polls/tests.py
@@ -5,21 +5,21 @@ from django.utils import timezone
 from polls.models import Question
 from django.urls import reverse
 
-# class QuestionModelTestCase(TestCase):#all test classes must end with TestCase or Tests
-#     def test_was_published_recently(self):#all test methods must begin with test_
-#         time = timezone.now() + datetime.timedelta(days=30)
-#         future_question = Question(pub_date=time)
-#         self.assertIs(future_question.was_published_recently(), False)
+class QuestionModelTestCase(TestCase):#all test classes must end with TestCase or Tests
+    def test_was_published_recently(self):#all test methods must begin with test_
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
     
-#     def test_was_published_recently_with_old_question(self):
-#         time = timezone.now()-datetime.timedelta(days=1, seconds=1)
-#         old_question = Question(pub_date=time)
-#         self.assertIs(old_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now()-datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
     
-#     def test_was_published_recently_with_recent_question(self):
-#         time = timezone.now()-datetime.timedelta(hours=23, minutes=59, seconds=59)
-#         recent_question = Question(pub_date=time)
-#         self.assertIs(recent_question.was_published_recently(), True)
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now()-datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
     
 
 def create_question(question_text, days):
@@ -32,7 +32,9 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
 
-        self.assertQuerySetEqual(response.context['lastest_question_list'], [])
+        self.assertQuerySetEqual(
+            response.context['lastest_question_list'], [],
+            )
 
     def test_past_questions(self):
         question = create_question(question_text="past question", days=-30)
@@ -54,4 +56,32 @@ class QuestionIndexViewTests(TestCase):
     def test_future_questions_and_past_questions(self):
         question = create_question(question_text="past questions", days=-30)
         create_question(question_text="Future question", days=30)
-        
+        response = self.client.get(reverse("polls:index"))
+
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'], [question],
+        )
+
+    def test_two_past_questions(self):
+        question1 = create_question(question_text='past question 1', days=-30)
+        question2 = create_question(question_text='past question 2', days=-5)
+
+        response = self.client.get(reverse("polls:index"))
+
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'], 
+            [question1, question2],
+        )
+
+class QuestionDetailViewTestCase(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question', days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_past_question(self):
+        past_question = create_question(question_text="past question", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
